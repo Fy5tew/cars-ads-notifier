d7424eab67a47739069493f226c20e0d9d3d4dac
Nikita <nikturchinovich@mail.ru>
2023-07-10 14:04:11 +0300

    Add kufar parser base

--- src/parsers/kufar.ts
+++ src/parsers/kufar.ts
@@ -1,27 +1,38 @@
+import { Browser } from '../browser';
+
 import { CarAd, IParser } from './types';
 
 
 export const parse = async (url: string, adsNumber = Infinity): Promise<CarAd[]> => {
-	return (
-		[
-			{
-				date: '',
-				title: '',
-				params: '',
-				photoURL: '',
-				year: '',
-				mileage: '',
-				location: '',
-				price: {
-					BYN: '',
-					USD: '',
-				},
-			},
-		]
-	);
+	return new Parser().parseAds(url, adsNumber);
 };
 
 
 export class Parser implements IParser {
-	
+	private browser: Browser;
+
+	constructor() {
+		this.browser = new Browser();
+	}
+
+	async parseAds(url: string, adsNumber = Infinity, closeBrowser = true): Promise<CarAd[]> {
+		await this.openBrowser();
+		try {
+			const parsedAds: CarAd[] = [];
+			return parsedAds;
+		}
+		finally {
+			if (closeBrowser) {
+				await this.closeBrowser();
+			}
+		}
+	}
+
+	async openBrowser() {
+		await this.browser.open();
+	}
+
+	async closeBrowser() {
+		await this.browser.close();
+	}
 }
