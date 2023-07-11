773bda7046465045c6225478b61b02862c8ab05c
Nikita <nikturchinovich@mail.ru>
2023-07-11 13:25:19 +0300

    Add interface IParserConstructor

--- src/types/IParser.ts
+++ src/types/IParser.ts
@@ -2,6 +2,11 @@ import { IBrowser } from './IBrowser';
 import { CarAd } from './CarAd';
 
 
+export interface IParserConstructor {
+    new (browser: IBrowser): IParser;
+}
+
+
 export interface IParser {
     browser: IBrowser;
     parseAds(url: string, adsNumber: number): Promise<CarAd[]>;
