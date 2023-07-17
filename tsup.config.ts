b245dcaa74eb93cda34ae17a7af5665837cbcb42
Nikita <nikturchinovich@mail.ru>
2023-07-17 17:03:59 +0300

    Add tsup config

---
+++ tsup.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+	// entry: ['src/**/*.ts'], // keep all files structure
+	entry: ['src/index.ts'], // single file
+	sourcemap: false,
+	clean: true,
+	minify: true,
+	treeshake: true,
+	outDir: 'build',
+});
